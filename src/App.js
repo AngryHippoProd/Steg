985b0ceb248abc3c0365293e75ecf896143a0871
AngryHippoProd <parnell@angryhippo.digital>
2022-08-18 14:03:26 -0400

    attempting upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
+import { FormControl, InputLabel , Input,
+         Card, } from '@mui/material';
+import Jello from './upload';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Stegenography
+        Steganography
       </header>
-
+      <FormControl className="form-container">
+        <InputLabel>Image Upload</InputLabel>
+      </FormControl>
+      <Jello></Jello>
     </div>
   );
 }
---
+++ src/encode.py
@@ -0,0 +1,2 @@
+import AddPhotoAlternateIcon from "@material-ui/icons/AddPhotoAlternate";
+import CollectionsIcon from "@material-ui/icons/Collections";
